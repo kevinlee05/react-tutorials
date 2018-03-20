bf54457e865eae77ee3412e43e48d4d9db6f76ae
kevinlee05 <kevinlee05@gmail.com>
2018-03-21 01:03:00 +0800

    add switch to reducer

--- valentino/src/js/reducers/index.js
+++ valentino/src/js/reducers/index.js
@@ -1,9 +1,21 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
     articles: []
 }
 
-//Reducers produce the state of the application.
+//Reducers produce the state of the application
 //initial state is passed as a default parameter.
-const rootReducer = (state = initialState, action) => state;
+//The reducer calculates the next state depending on the action type. Moreover, it should return at least the initial state when no action type matches.
+// When the action type matches a case clause the reducer calculates the next state and returns a new object.
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_ARTICLE:
+            state.articles.push(action.payload);
+            return state;
+        default:
+            return state;
+    }
+};
 
 export default rootReducer;
