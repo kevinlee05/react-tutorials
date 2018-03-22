f9cb3fdac1fc054b65f54d8020e15365b772d458
kevinlee05 <kevinlee05@gmail.com>
2018-03-22 22:40:08 +0800

    Form component

--- valentino/src/js/components/List.js
+++ valentino/src/js/components/List.js
@@ -5,9 +5,10 @@ const mapStateToProps = state => {
     return { articles: state.articles };
 };
 
+//The List component receives the prop articles which is a copy of the articlesarray. Such array lives inside the Redux state
 const ConnectedList = ({articles}) => (
     <ul className="list-group list-group-flush">
-        {articiles.map(el => (
+        {articles.map(el => (
             <li className="list-group-item" key={el.id}>
                 {el.title}
             </li>
@@ -16,5 +17,8 @@ const ConnectedList = ({articles}) => (
 )
 
 const List = connect(mapStateToProps)(ConnectedList);
+//the component gets exported as List. List is the result of connecting the stateless component ConnectedList with the Redux store.
+
+//A stateless component does not have its own local state. Data gets passed to it as props
 
 export default List;
