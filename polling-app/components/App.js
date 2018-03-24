84384f1ee2c0fa5a458046865bd975e672444cee
kevinlee05 <kevinlee05@gmail.com>
2018-03-24 12:05:10 +0800

    add socket.io and refactor App component

--- polling-app/components/App.js
+++ polling-app/components/App.js
@@ -1,8 +1,29 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.connect = this.connect.bind(this); // functions need to be bound to this when extending Component
+    }
+
+    componentWillMount(){
+        this.socket = io('http://localhost:3000');
+        this.socket.on('connect', this.connect);
+    }
+
+    connect(){
+        alert("Connected: " + this.socket.id);
+    }
+
     render(){ //ES6 shorthand for render = function()
-        return (<h1>Hello World from React</h1>);
+        return (
+            <div>
+                <h1>Hello World from React</h1>
+                <p>{this.socket.id}</p>
+            </div>
+        );
     }
 }
 
